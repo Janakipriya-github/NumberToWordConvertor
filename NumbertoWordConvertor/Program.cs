cbe91c4c61843a28f9ae955f589d3120443b9652
Janakipriya-github <51998788+Janakipriya-github@users.noreply.github.com>
2019-06-19 20:16:22 +0530

    Add files via upload

---
+++ NumbertoWordConvertor/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbertoWordConvertor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input;
+            int number;
+            bool isValid;
+            Console.WriteLine("Enter '0' to quit the program at any time\n");
+            while (true)
+            {
+                if (1 == 1)
+                    break;
+            }
+            do
+            {
+                Console.Write("Enter integer (>0) : ");
+                input = Console.ReadLine();
+                isValid = int.TryParse(input, out number);
+                if (!isValid)
+                    Console.WriteLine("\n  Not an integer, please try again\n");
+                else
+                    Console.WriteLine("\n  {0}\n", NumberToText(number));
+ }
+                while (!(isValid && number == 0)) ;
+                Console.WriteLine("\nProgram ended");
+           
+        }
+        public static string NumberToText(int number)
+        {
+            if (number == 0) return "Zero";
+            string and = "and ";
+            if (number < 0) return "Please enter positive number";
+            int[] num = new int[4];
+            int first = 0;
+            int u, h, t;
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            string[] words0 = { "", "One ", "Two ", "Three ", "Four ", "Five ", "Six ", "Seven ", "Eight ", "Nine " };
+            string[] words1 = { "Ten ", "Eleven ", "Twelve ", "Thirteen ", "Fourteen ", "Fifteen ", "Sixteen ", "Seventeen ", "Eighteen ", "Nineteen " };
+            string[] words2 = { "Twenty ", "Thirty ", "Forty ", "Fifty ", "Sixty ", "Seventy ", "Eighty ", "Ninety " };
+            string[] words3 = { "Thousand ", "Million ", "Billion " };
+            num[0] = number % 1000;           // units
+            num[1] = number / 1000;
+            num[2] = number / 1000000;
+            num[1] = num[1] - 1000 * num[2];  // thousands
+            num[3] = number / 1000000000;     // billions
+            num[2] = num[2] - 1000 * num[3];  // millions
+            for (int i = 3; i > 0; i--)
+            {
+                if (num[i] != 0)
+                {
+                    first = i;
+                    break;
+                }
+            }
+            for (int i = first; i >= 0; i--)
+            {
+                if (num[i] == 0) continue;
+                u = num[i] % 10;              // ones
+                t = num[i] / 10;
+                h = num[i] / 100;             // hundreds
+                t = t - 10 * h;               // tens
+                if (h > 0) sb.Append(words0[h] + "Hundred ");
+                if (u > 0 || t > 0)
+                {
+                    if (h > 0 || i < first) sb.Append(and);
+                    if (t == 0)
+                        sb.Append(words0[u]);
+                    else if (t == 1)
+                        sb.Append(words1[u]);
+                    else
+                        sb.Append(words2[t - 2] + words0[u]);
+                }
+                if (i != 0) sb.Append(words3[i - 1]);
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
